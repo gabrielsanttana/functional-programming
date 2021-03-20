7e862457ad955c7d6f57f7b9e2b0e07ce5279bf9
Gabriel Santana <gabriel.gsantana7@gmail.com>
2021-03-20 12:24:17 -0300

    Update pureFunctions.js

--- src/pureFunctions.js
+++ src/pureFunctions.js
@@ -5,18 +5,18 @@ function inpureGreet() {
   console.log('Hello, ' + name + '!');
 }
 
-inpureGreet();
+inpureGreet(); //Hello, World!
 
 name = 'Brazil';
 
-inpureGreet();
+inpureGreet(); //Hello, Brazil!
 
 //Pure:
 function pureGreet(name) {
   return 'Hello, ' + name + '!';
 }
 
-console.log(pureGreet('World'));
+console.log(pureGreet('World')); //Hello, World!
 
 //Not pure:
 let person = {name: 'Gabriel', lastName: 'Santana'};
@@ -25,11 +25,11 @@ function inpureChangeLastName(newLastName) {
   person.lastName = newLastName;
 }
 
-inpureChangeLastName('Gomes');
+inpureChangeLastName('Gomes'); // {name: 'Gabriel', lastName: 'Gomes'}
 
 //Pure:
 function pureChangeLastName(person, newLastName) {
   return {name: person.name, lastName: newLastName};
 }
 
-pureChangeLastName(person, 'Gomes');
+pureChangeLastName(person, 'Gomes'); //{name: 'Gabriel', lastName: 'Gomes'}
